b1dd834b7eb54d4981addae0607240472acecc0d
Shawn Wang <shawn.wang@striking.ly>
2018-05-16 15:39:32 +0800

    lint ignore readme

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['eslint-config-egg', 'plugin:prettier/recommended', ],
+  parserOptions: {
+    sourceType: 'module'
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  semi: false,
+  printWidth: 200,
+  singleQuote: true
+}
